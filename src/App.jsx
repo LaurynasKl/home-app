d03b7de129ceecb76ae46a020f538651863fac4d
Laurynas <klumbyslaurynas@gmail.com>
2024-10-01 19:54:32 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
-import CategoryList from './components/common/category/CategoryList'
-import Hero from './components/common/category/Hero'
-import TopBar from './components/layout/Topbar'
+import routes from './components/router/Router';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Services from './pages/Services';
+import Login from './pages/Login';
+import Error from './pages/Error';
+import { SiteLayout } from './components/layout/SiteLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <SiteLayout />, 
+    errorElement: <Error />, 
+    children: [
+      {
+        path: routes.home,
+        element: <Home />,
+      },
+      {
+        path: routes.about,
+        element: <AboutUs />,
+      },
+      {
+        path: routes.services,
+        element: <Services />,
+      },
+      {
+        path: routes.login,
+        element: <Login />,
+      },
+      // {
+      //   path: routes.searchCategory,
+      //   element: <SelectedCategory />,
+      // },
+    ]
+  }
+  
+]);
 
 function App() {
 
   return (
     <>
-    <TopBar />
-    <Hero />
-    <CategoryList />
+    <RouterProvider router={router} />
     </>
   )
 }
