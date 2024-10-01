887c8f436362cb9948ca39d07c69e858adc4c7b4
Laurynas <klumbyslaurynas@gmail.com>
2024-10-01 19:55:14 +0300

    puslapiu sukurimas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import CategoryList from "../components/common/category/CategoryList";
+import Hero from "../components/common/category/Hero";
+
+function Home(){
+    return(
+        <>
+        <Hero />
+        <CategoryList />
+        </>
+    )
+};
+
+export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom"
+
+function Login() {
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        navigate("/")
+    }
+    function handleSignIn() {
+        navigate("S")
+    }
+    return (
+        <div>
+            <h1>Login page</h1>
+            <button onClick={handleLogin}>login</button>
+            <p>If you dont have account signIn <button onClick={handleSignIn}>Sign In</button></p>
+        </div>
+    )
+}
+
+export default Login;
