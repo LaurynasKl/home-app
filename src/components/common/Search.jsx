4646c54c5b50962fd87be301c658ace2511b9108
Laurynas <klumbyslaurynas@gmail.com>
2024-10-01 19:06:56 +0300

    search

---
+++ src/components/common/Search.jsx
@@ -0,0 +1,21 @@
+import styles from './Search.module.scss';
+// import { CiSearch } from "react-icons/ci";
+
+function Search() {
+
+    return (
+        <div className={styles.search}>
+            <div>
+                <p>Find Home <span>Service/Repair</span> Near You</p>
+                <p className={styles.text}>Explore Best Home Service & Repair near you</p>
+            </div>
+            
+            <div className={styles.searchBtn}>
+                <input type="text" placeholder='Search'/>
+                {/* <button><CiSearch /></button> */}
+            </div>
+        </div>
+    )
+}
+
+export default Search;
